706c85b7e554b2ac282e561f481f48fcc82c9c71
DrCoffey <39605011+DrCoffey@users.noreply.github.com>
2025-06-25 21:28:54 -0700

    Big Clean Up
    preping for publication
    changed file names
    collapsed the stats files
    tested and should work :)

--- main_MouseSABehavior.m
+++ main_MouseSABehavior.m
@@ -18,34 +18,34 @@ cd(main_folder)
 addpath(genpath(main_folder))
 masterTable_flnm = '.\data_masterTable.mat'; % the masterTable .mat file loaded in if createNewMasterTable == false
 beh_datapath = {'.\All Behavior'}; % Used to generate a new masterTable if createNewMasterTable == true
-masterSheet_flnm = '.\Golden R01 Behavior Master Key.xlsx'; % Key describing information specific to each animal
-BE_intake_canonical_flnm = '.\2024.12.09.BE Intake Canonical.xlsx'; % Key for drug concentration, dose, and intake only used if runType == 'BE'
-experimentKey_flnm = '.\Experiment Key.xlsx'; % Key for
+masterSheet_flnm = '.\Subject Key.xlsx'; % Key describing information specific to each animal
+BE_intake_canonical_flnm = '.\BE Measured Intake.xlsx'; % Data table for measured intake only used if runType == 'BE'
+experimentKey_flnm = '.\Experiment Key.xlsx'; % Key for date, session type, experiment type, drug concentration, dose.
 
 % MISC. SETTINGS
-runNum = '1_2_3_4_6'; % options: 'all' or desired runs separated by underscores (e.g. '1', '1_3_4', '3_2')
-runType = 'All'; % options: 'ER' (Extinction Reinstatement), 'BE' (Behavioral Economics), 'SA' (Self Administration), 'All' (All)
+runNum = '1_2_3_4_6'; % options: 'all' or desired runs separated by underscores. Must match numbers in Experiment Key (e.g. '1', '1_3_4', '3_2')
+runType = 'All'; % options: 'ER' (SA & Extinction & Reinstatement), 'BE' (Behavioral Economics), 'SA' (Self Administration), 'All' (All run seperately)
 createNewMasterTable = false; % true: generates & saves a new master table from medPC files in datapath. false: reads mT in from masterTable_flnm if set to false, otherwise 
 firstHour = true; % true: acquire data from the first-hour of data and analyze in addition to the full sessions (Important for camparing behavior to 1h reinstatement session).
-excludeData = true; % true: excludes data based on the 'RemoveSession' column of masterSheet
-acquisition_thresh = 10; % to be labeled as "Acquire", animal must achieve an average number of infusions in the second weak of Training sessions greater than this threshold
+excludeData = true; % true: excludes data based on the 'RemoveSession' column of Subject Key
+acquisition_thresh = 10; % to be labeled as "Acquire", animal must achieve an average number of infusions in Training sessions greater than this threshold
 acquisition_testPeriod = {'Training', 'last', 10}; % determines sessions to average infusions across before applying acquisition_thresh. second value can be 'all', 'first', or 'last'. if 'first' or 'last', there should be a 3rd value giving the number of days to average across, or it will default to 1. 
 pAcq = true; % true: plot aquisition histogram to choose threshold 
 interpWeights = false; % true: interpolate daily weights from weekly weights
 interpWeight_sessions = [1,6,11,16,21]; % [sessions with true weights for interpolation]
 
-run_BE_analysis = true;
-run_withinSession_analysis = true;
-run_individualSusceptibility_analysis = true;
+run_BE_analysis = true; % Run Behavioral Economics analysis?
+run_withinSession_analysis = true; % Run within-sessions analysis?
+run_individualSusceptibility_analysis = true; % Run individual risk analysis?
 
 % FIGURE OPTIONS
 % Currently, if figures are generated they are also saved. 
 saveTabs = true; % true: save matlab tables of analyzed datasets
 dailyFigs = false; % true: generate daily figures from dailySAFigures.m
 pubFigs = true; % true: generate publication figures from pubSAFigures.m
-indivIntake_figs = false; % true: generate figures for individual animal behavior across & within sessions
+indivIntake_figs = false; % true: generate figures for individual animal behavior across & within sessions (useful for troubleshooting but produces a lot of figures)
 groupIntake_figs = true; % true: generate figures grouped by sex, strain, etc. for animal behavior across & within sessions
-groupOralFentOutput_figs = true; % true: generate severity figures
+severity_figs = true; % true: generate severity figures
 figsave_type = {'.png','.fig'}; % file types for figure outputs. May be any MATLAB standard image or figure type
 
 % color settings chosen for publication figures.
@@ -85,7 +85,7 @@ end
 % Import Master Key
 opts = detectImportOptions(masterSheet_flnm);
 opts = setvartype(opts,{'TagNumber','ID','Cage','Sex','Strain','TimeOfBehavior'},'categorical'); % Must be variables in the master key
-mKey=readtable('Golden R01 Behavior Master Key.xlsx',opts);
+mKey=readtable(masterSheet_flnm,opts);
 
 % Create subdirectories
 toMake = {tabs_savepath, dailyfigs_savepath, pubfigs_savepath, ...
@@ -115,12 +115,13 @@ end
 % get index for different experiments 
 dex = getExperimentIndex(mT, runNum, runType);
 
-% hackymakelifebetterlater
+% Check to be sure run types that are requested actually exist in the dataset
 if any(contains(fieldnames(dex), 'ER'))
     if isempty(dex.ER)
         runType(runType == 'ER') = [];
     end
 end
+
 if any(contains(fieldnames(dex), 'BE'))
     if isempty(dex.BE)
         runType(runType == 'BE') = [];
@@ -197,13 +198,13 @@ if any(ismember(runType, 'BE')) && run_BE_analysis
 end
 
 %% Within Session Behavioral Analysis 
-
 if run_withinSession_analysis
     fig_colors = {[.5,.5,.5], col_F_c57, col_M_c57, col_F_CD1, col_M_CD1};
     [mTDL, mPressT, mDrugsLT] = WithinSession_Processes(mT, dex, sub_dir, indivIntake_figs, indivIntakefigs_savepath, groupIntake_figs, groupIntakefigs_savepath, saveTabs, tabs_savepath, figsave_type,fig_colors);
 end
 
 %% Statistic Linear Mixed Effects Models
+% This section is not very flexible and should be modified for different experiments
 statsname=[sub_dir, tabs_savepath, 'Oral SA Group Stats '];
 
 % SA Acquitition All Animals with SA (Figure 1)
@@ -228,16 +229,14 @@ if any(ismember(runType,'SA'))
     end
 end
 
-
 if any(ismember(runType,'ER'))
-    
     % LMEs for Extinction and Reinstatment Experiment (Figure 2)
     % Overall LMEs for Self Administration (not reported)
+    ER_Stats=struct;
     data = mT(dex.ER,:);
     data=data(data.Acquire == 'Acquire',:);
     data=data(data.Session<=15,:);
     dep_var = ["Intake", "HeadEntries", "Latency", "ActiveLever", "InactiveLever"];
-
     if ismember('Strain', data.Properties.VariableNames) & ismember('Sex', data.Properties.VariableNames)
         lme_form = " ~ Sex*Strain*Session + (1|TagNumber)"
     elseif ismember('Sex', data.Properties.VariableNames)
@@ -247,108 +246,80 @@ if any(ismember(runType,'ER'))
     end
 
     if ~isempty(data)
-        ER_SA_LMEstats = getLMEstats(data, dep_var, lme_form);
-        if saveTabs
-            save([statsname, 'ER'], 'ER_SA_LMEstats');
-        end
+       ER_Stats.SA_LME = getLMEstats(data, dep_var, lme_form);
     end
     
     % Idividual Strains over Sessions & Session 15 Comparison (Figure 2)
-    if true
-        datac57=data(data.Strain=='c57',:);
-        dataCD1=data(data.Strain=='CD1',:);
-        lme_form = " ~ Sex*Session + (Session|TagNumber)";
-        ER_SA_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
-        ER_SA_CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
-        save([statsname, 'ER'], 'ER_SA_c57_LMEstats');
-        save([statsname, 'ER'], 'ER_SA_CD1_LMEstats');
-        % Session 15
-        datac57=datac57(datac57.Session==15,:);
-        dataCD1=dataCD1(dataCD1.Session==15,:);
-        lme_form = " ~ Sex + (Session|TagNumber)";
-        ER_S15_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
-        ER_S15_CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
-        save([statsname, 'ER'], 'ER_S15_c57_LMEstats');
-        save([statsname, 'ER'], 'ER_S15_CD1_LMEstats');
-    end
+    datac57=data(data.Strain=='c57',:);
+    dataCD1=data(data.Strain=='CD1',:);
+    lme_form = " ~ Sex*Session + (Session|TagNumber)";
+    ER_Stats.SA_c57_LME = getLMEstats(datac57, dep_var, lme_form);
+    ER_Stats.SA_CD1_LME = getLMEstats(dataCD1, dep_var, lme_form);
 
+    % Session 15
+    datac57=datac57(datac57.Session==15,:);
+    dataCD1=dataCD1(dataCD1.Session==15,:);
+    lme_form = " ~ Sex + (Session|TagNumber)";
+    ER_Stats.S15_c57_LME = getLMEstats(datac57, dep_var, lme_form);
+    ER_Stats.S15_CD1_LME = getLMEstats(dataCD1, dep_var, lme_form);
+  
     % Overall LMEs for Extinction (not reported)
     data = mT(dex.ER,:);
     data=data(data.Acquire == 'Acquire',:);
     data=data(data.sessionType=='Extinction',:);
     dep_var = ["HeadEntries", "Latency", "ActiveLever", "InactiveLever"];
-
     if ismember('Strain', data.Properties.VariableNames) & ismember('Sex', data.Properties.VariableNames)
-        lme_form = " ~ Sex*Strain*Session + (1|TagNumber)"
+        lme_form = " ~ Sex*Strain*Session + (1|TagNumber)";
     elseif ismember('Sex', data.Properties.VariableNames)
         lme_form = " ~ Sex*Session + (1|TagNumber)";
     elseif ismember('Strain', data.Properties.VariableNames)
         lme_form = " ~ Strain*Session + (1|TagNumber)";
     end
-
-    if ~isempty(data)
-        ER_Ext_LMEstats = getLMEstats(data, dep_var, lme_form);
-        if saveTabs
-            save([statsname, 'ER'], 'ER_Ext_LMEstats');
-        end
-    end
+    ER_Stats.Ext_LMEstats = getLMEstats(data, dep_var, lme_form);
 
     % Idividual Strains over Sessions Extinction (Figure 2)
-    if true
-        datac57=data(data.Strain=='c57',:);
-        dataCD1=data(data.Strain=='CD1',:);
-        lme_form = " ~ Sex*Session + (Session|TagNumber)";
-        ER_Ext_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
-        ER_Ext_CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
-        save([statsname, 'ER'], 'ER_Ext_c57_LMEstats');
-        save([statsname, 'ER'], 'ER_Ext_CD1_LMEstats');
-    end
-
+    datac57=data(data.Strain=='c57',:);
+    dataCD1=data(data.Strain=='CD1',:);
+    lme_form = " ~ Sex*Session + (Session|TagNumber)";
+    ER_Stats.Ext_c57_LME = getLMEstats(datac57, dep_var, lme_form);
+    ER_Stats.Ext_CD1_LME = getLMEstats(dataCD1, dep_var, lme_form);
 
   % Overall LMEs for Reinstatemet (not reported)
     data = hmT(dex.ER,:);
     data=data(data.Acquire == 'Acquire',:);
     data=data(data.sessionType=='Reinstatement' | data.Session==25,:);
     dep_var = ["HeadEntries", "Latency", "ActiveLever", "InactiveLever"];
-
     if ismember('Strain', data.Properties.VariableNames) & ismember('Sex', data.Properties.VariableNames)
-        lme_form = " ~ Sex*Strain*Session + (1|TagNumber)"
+        lme_form = " ~ Sex*Strain*Session + (1|TagNumber)";
     elseif ismember('Sex', data.Properties.VariableNames)
         lme_form = " ~ Sex*Session + (1|TagNumber)";
     elseif ismember('Strain', data.Properties.VariableNames)
         lme_form = " ~ Strain*Session + (1|TagNumber)";
     end
-
-    if ~isempty(data)
-        ER_Rei_LMEstats = getLMEstats(data, dep_var, lme_form);
-        if saveTabs
-            save([statsname, 'ER'], 'ER_Rei_LMEstats');
-        end
-    end
+    ER_Stats.Rei_LME = getLMEstats(data, dep_var, lme_form);
 
     % Idividual Strains over Sessions Reinstatement (Figure 2)
-    if true
-        datac57=data(data.Strain=='c57',:);
-        dataCD1=data(data.Strain=='CD1',:);
-        lme_form = " ~ Sex*Session + (Session|TagNumber)";
-        ER_Rei_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
-        ER_Rei_CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
-        save([statsname, 'ER'], 'ER_Rei_c57_LMEstats');
-        save([statsname, 'ER'], 'ER_Rei_CD1_LMEstats');
-        % Session 26
-        datac57=datac57(datac57.sessionType=='Reinstatement',:);
-        dataCD1=dataCD1(dataCD1.sessionType=='Reinstatement',:);
-        lme_form = " ~ Sex + (Session|TagNumber)";
-        ER_SR_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
-        ER_SR_CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
-        save([statsname, 'ER'], 'ER_SR_c57_LMEstats');
-        save([statsname, 'ER'], 'ER_SR_CD1_LMEstats');
-    end
+    datac57=data(data.Strain=='c57',:);
+    dataCD1=data(data.Strain=='CD1',:);
+    lme_form = " ~ Sex*Session + (Session|TagNumber)";
+    ER_Stats.Rei_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
+    ER_Stats.Rei_CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
 
+    % Session 26
+    datac57=datac57(datac57.sessionType=='Reinstatement',:);
+    dataCD1=dataCD1(dataCD1.sessionType=='Reinstatement',:);
+    lme_form = " ~ Sex + (Session|TagNumber)";
+    ER_Stats.sexRei_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
+    ER_Stats.sexRei_CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
+    
+    if saveTabs
+       save([statsname, 'ER'], 'ER_Stats');
+    end
 end
 
 if any(ismember(runType,'BE'))
     % Behavioral Economics SA Sessions
+    BE_Stats=struct;
     data = mT(dex.BE,:);
     data=data(data.Acquire == 'Acquire',:);
 
@@ -358,18 +329,17 @@ if any(ismember(runType,'BE'))
     datac57=data(data.Strain=='c57',:);
     dataCD1=data(data.Strain=='CD1',:);
     lme_form = " ~ Sex*Session + (Session|TagNumber)";
-    BE_SA_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
-    BE_SA_CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
-    save([statsname, 'BE'], 'BE_SA_c57_LMEstats');
-    save([statsname, 'BE'], 'BE_SA_CD1_LMEstats');
+    BE_Stats.SA_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
+    BE_Stats.CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
+
     % Session 15
     datac57=datac57(datac57.Session==15,:);
     dataCD1=dataCD1(dataCD1.Session==15,:);
     lme_form = " ~ Sex + (Session|TagNumber)";
-    BE_S15_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
-    BE_S15_CD1_LMEstats = getLME
-
-     % Behavioral Economics BE Sessions
+    BE_Stats.S15_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
+    BE_Stats.S15_CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
+    
+    % Behavioral Economics BE Sessions
     data = beT;
     % Individual Strain for SA (BE Experiment)
     data.Concentration=categorical(data.Concentration);
@@ -378,10 +348,12 @@ if any(ismember(runType,'BE'))
     datac57=data(data.Strain=='c57',:);
     dataCD1=data(data.Strain=='CD1',:);
     lme_form = " ~ Sex*Concentration + (1|TagNumber)";
-    BE_BE_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
-    BE_BE_CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
-    save([statsname, 'BE'], 'BE_BE_c57_LMEstats');
-    save([statsname, 'BE'], 'BE_BE_CD1_LMEstats');
+    BE_Stats.BE_c57_LMEstats = getLMEstats(datac57, dep_var, lme_form);
+    BE_Stats.BE_CD1_LMEstats = getLMEstats(dataCD1, dep_var, lme_form);
+    
+    if saveTabs
+       save([statsname, 'BE'], 'BE_Stats');
+    end
 end
 
 %% Individual Variability Suseptibility Modeling
@@ -395,18 +367,16 @@ end
 %       SSnote: add calculation of PCA for subgroups
 %
 % Individual Susceptibility Metrics
-%   1) Intake = total fentanyl consumption in SA (ug/kg)
-%   2) Seeking = total head entries in SA
-%   3) Cue Association = HE Latency in SA 
-%   4) Escalation = Slope of intake in SA
+%   1) Intake = total fentanyl consumption in SA training (ug/kg)
+%   2) Seeking = total head entries in SAtraining
+%   3) Cue Association = Average HE Latency in SA 
+%   4) Escalation = slope of intake in SA (session 1-10)
 %   5) Extinction = total active lever presses during extinction
 %   6) Persistance = slope of extinction active lever presses
 %   7) Flexibility = total inactive lever presses during extinction
 %   8) Relapse = total presses during reinstatement
-%   9) Cue Recall = HE Latency in reinstatement 
+%   9) Cue Recall = Average HE Latency in reinstatement 
 %
-% how does it affect severity that negative z-scores are included?
-% 
 % Experiment-dependent use cases: 
 %   If the experiment contains ER experiment data, animals involved in these
 %       experiments will be analyzed with respect to all IS metrics. 
@@ -420,7 +390,6 @@ end
 %                   (separately calculated and saved for ER and non ER groups)
 
 if run_individualSusceptibility_analysis
-
     % subgroups of z-scored data to run correlations across
     corrGroups = {{{'all'}}, ...
                   {{'Strain', 'c57'}}, ...
@@ -437,17 +406,9 @@ if run_individualSusceptibility_analysis
     violSubsets = {{'all'}, {'all'}, {'Strain', 'c57'}, {'Strain', 'CD1'}};
     violGroups = {'Strain', 'Sex', 'Sex', 'Sex'};
     violLabels = {'Strain', 'Sex', 'c57 Sex', 'CD1 Sex'};
-
     pcaGroups = corrGroups;
-
     ivT = IS_processes(mT, dex, runType, corrGroups, violSubsets, ...
                        violGroups, violLabels, pcaGroups, sub_dir, ...
-                       saveTabs, tabs_savepath, groupOralFentOutput_figs, ...
+                       saveTabs, tabs_savepath, severity_figs, ...
                        groupOralFentOutput_savepath, figsave_type);
-    % if firstHour
-    %     fH_ivT = IS_processes(hmT, dex, runType, corrGroups, violSubsets, ...
-    %                           violGroups, violLabels, pcaGroups, fH_sub_dir, ...
-    %                           saveTabs, tabs_savepath, groupOralFentOutput_figs, ...
-    %                           groupOralFentOutput_savepath, figsave_type);
-    % end
 end
